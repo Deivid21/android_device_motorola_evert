f3b27bbcf35fabd50b4aa2572018f1bb66993602
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-24 13:26:12 -0300

    Revert "evert: Use minimal GMS if building with it"
    This reverts commit 7d8f141b3629489b08e19b7e9064fe1e395cd899.
    
    Change-Id: I0196d75f287b8321b3567b0b09e77186287307ae

--- device.mk
+++ device.mk
@@ -69,12 +69,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += qcom.fmradio
 
-# GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_minimal.mk
-WITH_GMS_COMMS_SUITE=false
-endif
-
 # Init
 PRODUCT_PACKAGES += \
     init.evert.rc
